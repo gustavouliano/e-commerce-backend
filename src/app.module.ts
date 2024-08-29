295d2f60549ead4ac38b26704f49efc04e714f63
Gustavo Uliano <gustavouliano11@gmail.com>
2024-08-29 16:43:08 -0300

    environment variables configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,20 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductCategory } from './modules/product-categories/entities/product-category';
 import { ProductCategoriesModule } from './modules/product-categories/product-categories.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5433,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'ecommerce',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT, 10),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASS,
+            database: process.env.DATABASE_NAME,
             entities: [ProductCategory],
             synchronize: true,
         }),
